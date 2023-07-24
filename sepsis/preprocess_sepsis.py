53cf3e9b1c3707ffaa7e7334baac459a6f6fc50c
Yinghao Zhu <yhzhu99@gmail.com>
2023-07-25 04:02:49 +0800

    refactor(sepsis): tidy up preprocessing script code

--- sepsis/preprocess_sepsis.py
+++ sepsis/preprocess_sepsis.py
@@ -2,22 +2,23 @@ import pandas as pd
 import os
 
 
-def generate_dataset(data_file_path, set):
-    combined_data = pd.DataFrame()
-    psv_folder = data_file_path  
+def generate_df(data_path):
+    dataframes  = []
 
-    for filename in os.listdir(psv_folder):
+    for filename in os.listdir(data_path):
+        print("[Processing file:]", filename)
         if filename.endswith(".psv"):
-            filepath = os.path.join(psv_folder, filename)
+            filepath = os.path.join(data_path, filename)
             data = pd.read_csv(filepath, sep='|', skipinitialspace=True)
             
             # get the ID and label
-            patient_id = int(filename[1:7])
+            patient_id = str(filename[1:7])
             sepsis_label = data['SepsisLabel'].values
             data['PatientID'] = patient_id
             data['SepsisLabel'] = sepsis_label
 
-            combined_data = combined_data.append(data, ignore_index=True)
+            dataframes.append(data)
+    combined_data = pd.concat(dataframes, ignore_index=True)
 
     # adjust the order and titles of some columns
     columns_order = ['PatientID', 'SepsisLabel', 'ICULOS', 'Gender', 'Age', 'HospAdmTime', 'Unit1', 'Unit2'] + [col for col in combined_data.columns if col not in ['PatientID', 'SepsisLabel', 'ICULOS', 'Gender', 'Age', 'HospAdmTime', 'Unit1', 'Unit2']]
@@ -25,21 +26,14 @@ def generate_dataset(data_file_path, set):
 
     combined_data = combined_data.rename(columns={'Gender': 'Sex'})
     combined_data = combined_data.rename(columns={'SepsisLabel': 'Outcome'})
-
-    if set == 1:
-        combined_data.to_csv('training_dataA.csv', index=False)
-    else:
-        combined_data.to_csv('training_dataB.csv', index=False)
-
+    
+    return combined_data
 
 def main():
-    # Replace with your PSV folder path
-    data_file_path1 = 'training_setA'
-    generate_dataset(data_file_path1, set=1)
-
-    data_file_path2 = 'training_setB'
-    generate_dataset(data_file_path2, set=2)
-
+    df_a = generate_df(data_path='raw/training_setA')
+    df_a.to_csv('./processed/sepsis_seta.csv', index=False)
+    df_b = generate_df(data_path='raw/training_setB')
+    df_b.to_csv('./processed/sepsis_setb.csv', index=False)
 
 if __name__ == '__main__':
     main()
